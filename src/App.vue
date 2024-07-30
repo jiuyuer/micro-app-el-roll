6c4ae270c4a168999b9d87e945e85ed43c67eeea
mitu <jiuyue14@163.com>
2024-07-30 22:43:50 +0800

    基础框架

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <nav class="nav">
+    <RouterLink to="/">home</RouterLink>
+    <RouterLink to="/vue2-demo">vue2-demo</RouterLink>
+    <RouterLink to="/vue3-demo">vue3-demo</RouterLink>
+  </nav>
+  <main class="main">
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+.nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80px;
+  a {
+    font-size: 16px;
+    margin: 0 20px;
+    &:hover {
+      color: blue;
+    }
+  }
+}
+.main {
+  height: calc(100vh - 40px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
